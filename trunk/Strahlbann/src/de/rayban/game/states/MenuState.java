eb63e126bec46ed79f860a289cb4543c7cdce80c
stefanstuetzer <stefanstuetzer@e5b2ebe7-3b1e-443f-6422-6d9a185c1d94>
2011-01-11 09:56:03 +0000

    save actions

--- trunk/Strahlbann/src/de/rayban/game/states/MenuState.java
+++ trunk/Strahlbann/src/de/rayban/game/states/MenuState.java
@@ -22,73 +22,73 @@ import de.rayban.game.StrahlBann;
  * @author Stefan
  */
 public class MenuState extends StrahlBannGameState {
-	
+
 	/** menu entries to display */
 	public List<String> menuOptions = Arrays.asList("Start Game", "Options", "High Score", "About StrahlBann", "Quit Game");
 
 	/** background image of main menu */
 	private Image background = null;
-	
+
 	// TODO stefan durch UniCodeFont ersetzen
-	TrueTypeFont entryFont; 
-	TrueTypeFont infoFont; 
-	
-	public MenuState(int id) {
+	TrueTypeFont entryFont;
+	TrueTypeFont infoFont;
+
+	public MenuState(final int id) {
 		super(id);
 	}
 
 	@Override
-	public void init(GameContainer container, StrahlBann game) {
+	public void init(final GameContainer container, final StrahlBann game) {
 		try {
-			background = new Image("data/images/menu_background.png");
-		
+			background = new Image("resources/images/menu_background.png");
+
 			entryFont = new TrueTypeFont(new Font("Verdana", Font.BOLD, 25), true);
 			infoFont= new TrueTypeFont(new Font("Verdana", Font.ITALIC, 10), true);
-		} catch (SlickException e) {
+		} catch (final SlickException e) {
 			Log.error("Could not find backgrond image.", e);
 		}
 	}
 
 	@Override
-	public void render(GameContainer container, StrahlBann game, Graphics g) {
+	public void render(final GameContainer container, final StrahlBann game, final Graphics g) {
 		// draw background
 		background.draw(0,0);
-		
+
 		// draw menu options
 		int yPos = 100;
-		for (String menuEntry : menuOptions) {
+		for (final String menuEntry : menuOptions) {
 			entryFont.drawString(150, yPos, menuEntry);
 			yPos+=50;
 		}
-		
+
 		// draw info
 		infoFont.drawString(10, container.getHeight() -20, "by Daniel Ludwig and Stefan Stützer");
 	}
 
 	@Override
-	public void update(GameContainer container, StrahlBann game, int delta) {
+	public void update(final GameContainer container, final StrahlBann game, final int delta) {
 		container.getInput();
-		
-		
+
+
 	}
-	
+
 	@Override
-	public void keyReleased(int key, char c) {
+	public void keyReleased(final int key, final char c) {
 		super.keyReleased(key, c);
 
 		if (key == Input.KEY_1) {
 			game.enterState(StrahlBann.IN_GAME_STATE, new FadeOutTransition(Color.cyan), new FadeInTransition(Color.blue));
 		}
-		
+
 		if (key == Input.KEY_ESCAPE) {
 			Log.info("Quit Strahlbann");
 			System.exit(1);
 		}
-		
+
 		if (key == Input.KEY_F2) {
 			try {
 				container.setFullscreen(!container.isFullscreen());
-			} catch (SlickException e) {
+			} catch (final SlickException e) {
 				Log.error(e);
 			}
 		}
