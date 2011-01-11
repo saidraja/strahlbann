4ac6714870ae3d0a0908d7a9e719b47584e1b727
stefanstuetzer <stefanstuetzer@e5b2ebe7-3b1e-443f-6422-6d9a185c1d94>
2011-01-11 09:56:03 +0000

    save actions

--- Strahlbann/src/de/rayban/game/states/InGameState.java
+++ Strahlbann/src/de/rayban/game/states/InGameState.java
@@ -7,52 +7,49 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
 
-import de.rayban.core.EntityManager;
 import de.rayban.core.logic.GameLogic;
 import de.rayban.game.StrahlBann;
 import de.rayban.game.actors.Player;
-import de.rayban.game.actors.Text;
 
 /**
  * Hier wird das Spiel ansich gerendert.
- * 
- * @author Daniel
  *
+ * @author Daniel
  */
 public class InGameState extends StrahlBannGameState {
-	
+
 	private GameLogic gameLogic;
-	
-	public InGameState(int id) {
+
+	public InGameState(final int id) {
 		super(id);
 	}
 
 	@Override
-	public void render(GameContainer container, StrahlBann game, Graphics g) {
+	public void render(final GameContainer container, final StrahlBann game, final Graphics g) {
 		g.drawString("InGame", 100, 10);
 		g.drawString("Score: " + gameLogic.currentScore(), 100, 50);
 	}
 
 	@Override
-	public void update(GameContainer container, StrahlBann game, int delta){
+	public void update(final GameContainer container, final StrahlBann game, final int delta){
 		gameLogic.update(delta);
 	}
 
 	@Override
-	public void init(GameContainer container, StrahlBann game) {
+	public void init(final GameContainer container, final StrahlBann game) {
 		gameLogic = GameLogic.start(game.getEntityManager());
-		
-		int height = container.getHeight();
-    	int width = container.getWidth();
-		
-    	Player p = new Player(40, width/2, height, Color.cyan);
+
+		final int height = container.getHeight();
+    	final int width = container.getWidth();
+
+    	final Player p = new Player(40, width/2, height, Color.cyan);
     	p.setStateVisibility(StrahlBann.IN_GAME_STATE);
-    	
+
     	game.getEntityManager().add(p);
 	}
-	
+
 	@Override
-	public void keyReleased(int key, char c) {
+	public void keyReleased(final int key, final char c) {
 		super.keyReleased(key, c);
 		if (key == Input.KEY_ESCAPE) {
 			game.enterState(StrahlBann.MAIN_MENU_STATE, new FadeOutTransition(Color.white), new FadeInTransition(Color.black));
