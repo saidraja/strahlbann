bf032ea498540d7cb4257237dd457d5e65326d94
stefanstuetzer <stefanstuetzer@e5b2ebe7-3b1e-443f-6422-6d9a185c1d94>
2011-01-17 19:05:50 +0000

    level and some comments

--- trunk/Strahlbann/src/de/rayban/core/level/LevelLoader.java
+++ trunk/Strahlbann/src/de/rayban/core/level/LevelLoader.java
@@ -6,7 +6,7 @@ package de.rayban.core.level;
  */
 public class LevelLoader {
 
-	public static Level loadLevel(final int level) {
+	public static Level loadLevel(final int level) throws LevelLoadException {
 		// TODO Level Daten aus Datei laden
 		final LevelImpl levelImpl = new LevelImpl();
 		levelImpl.setLevel(1);
